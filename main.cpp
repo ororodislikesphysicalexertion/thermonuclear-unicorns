a4cdbf789b2e4b68ce2ce59fdb0b4961a29b970e
ororodislikesphysicalexertion <oronihasan@gmail.com>
2020-01-22 23:02:35 +0300

    gayz

--- main.cpp
+++ main.cpp
@@ -11,18 +11,36 @@
 #include "misc.hpp"
 #include <kipr/botball.h>
 
+void initial();
+
 int main(int argc, const char * argv[]) {
     
-    // turning function testing
-    
-    gyro rawr;
-    rawr.angle_turn(90);
-    rawr.angle_turn(45);
-    rawr.angle_turn(45);
-    std::cout << "Print gyro value: " << rawr.gyro_r() << std::endl;
+    initial();
+    demobotSeedings();
+   
+  //  std::cout << "Print gyro value: " << rawr.gyro_r() << std::endl;
     
     
     return 0;
+}
+
+void demobotSeedings(){
+    
+    rawr.angle_turn(); // rotate so that demobot is towards coupler
+    rawr.straight_angle(20, 200); // driving towards coupler
+    rawr.angle_turn(pi/4); // pulling coupler out from beneath
+    rawr.straight_angle(20, -200); // backs up with coupler
+    rawr.angle_turn(pi); // rotates with couler towards water poms
+    rawr.straight_angle(40, 300); // drives straight up to the water pom ledge
+
+    rawr.straight_angle(40, -300); // goes back until it is bad at the ramp entrance
+    rawr.angle_turn(pi/4);
+    rawr.straight_angle(10, 150);
+    rawr.angle_turn(-pi/4);
+    rawr.straight_angle(35, 250); // goes up the ramp
+    rawr.angle_turn(-pi/4); // dp I want to use this or a touch sensor? anyways, demobot turns to go on bridge
+    rawr.straight_angle(30, 200); // drives straight slowly while a mechanism pushes water poms into coupler below
+    
 }
 
 //create hard code
