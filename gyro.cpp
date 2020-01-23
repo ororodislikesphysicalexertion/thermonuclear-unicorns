d8f9cc79e77a3fec24c6560bac7972e858b5fad2
ororodislikesphysicalexertion <45035470+ororodislikesphysicalexertion@users.noreply.github.com>
2020-01-23 21:23:34 +0300

    Update gyro.cpp

--- gyro.cpp
+++ gyro.cpp
@@ -151,23 +151,33 @@ using n_time = std::chrono::high_resolution_clock;
 
     //i hope this one works and makes sense
     void gyro::angle_straight(int distance, int speed){ //enter distance in cm, it gets multiplied by 95.1 to convert to KIPR units
-        double TO_CM = 95.1; //found motor position ticks per centimeter
-        int time = (TO_CM * distance) / speed;
-        int timeConversion = 1000;
-        const int initialAngle = 0;
-        int differenceAngle = std::abs(initialAngle - gyro_z());
+    	double TO_CM = 95.1; //found motor position ticks per centimeter
+        int time = ((TO_CM * distance) / speed) * 1000; // milliseconds
+        double currentAngle = 0;
+    //    int differenceAngle = std::abs(initialAngle - gyro_z());
+        
+        while(time < duration()){
         
         if(differenceAngle > initialAngle){
-            double speed_1 = speed * (1 + correction(differenceAngle)); //test correction(differenceAngle)
-            double speed_2 = speed * (1 - correction(differenceAngle));// maybe try with normal value first
-            drive(speed_1, speed_2);
-            msleep(time * timeConversion);
+        	double speed_1 = speed * (1 + correction(currentAngle)); //test correction(differenceAngle)
+        	double speed_2 = speed * (1 - correction(currentAngle));// maybe try with normal value first
+        	drive(speed_1, speed_2);
+            
         }
         
         if(differenceAngle < initialAngle){
-            double speed_1 = speed * (1 - correction(differenceAngle));
-            double speed_2 = speed * (1 + correction(differenceAngle));
-            drive(speed_1, speed_2);
-            msleep(time * timeConversion);
+        	double speed_1 = speed * (1 - correction(currentAngle));
+        	double speed_2 = speed * (1 + correction(currentAngle));
+        	drive(speed_1, speed_2);
+            
+        }
+            msleep(time);
+            currentAngle += std::abs(gyro_z()) * 10;
         }
     }
+
+    
+    
+
+
+
