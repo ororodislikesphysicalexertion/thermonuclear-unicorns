e848552036673c1383a9e10bde8b4fd2bd05a3b5
ororodislikesphysicalexertion <45035470+ororodislikesphysicalexertion@users.noreply.github.com>
2020-01-26 18:31:36 +0300

    Update gyro.cpp

--- gyro.cpp
+++ gyro.cpp
@@ -15,12 +15,29 @@
 #include <numeric>
 #include <chrono>
 
-//finding the bias
+
+
+//clock stuff
 
 using n_time = std::chrono::high_resolution_clock;
     n_time::time_point c_time = n_time::now();
-    n_time::time_point start;
+    n_time::time_point start = n_time::now();
 
+
+auto gyro::duration(){
+        
+        return std::chrono::duration_cast<std::chrono::milliseconds>(c_time-start).count();
+    }
+
+
+
+//	auto currentTime = std::chrono::system_clock::now();
+//	auto cTime = std::chrono::time_point_cast<std::chrono::milliseconds>(currentTime);
+
+//----------------------------------------------------------------------
+
+
+	//finding the bias
     
     int gyro::gyro_r(){
         double n_readings = 100;
@@ -45,15 +62,15 @@ using n_time = std::chrono::high_resolution_clock;
     
 //----------------------------------------------------------------------
     
-    int gyro::angle_add (){
+    int gyro::angle_add (){ //fix
         
-        double angle_pos = std::abs((gyro_z() - average)) * 10;
+        double angle_pos = std::abs(gyro_z() - average) * 10;
         return angle_pos;
     }
     
 //----------------------------------------------------------------------
 
-    // this one kinda sucks
+
     void gyro::turn(double t_theta, int l_speed, int r_speed){ //fix
         
         double theta = 0.0;
@@ -61,47 +78,17 @@ using n_time = std::chrono::high_resolution_clock;
         mav(3, r_speed);
         do{
             msleep(10);
-            theta += angle_add();
+            theta += angle_add();  
         }
-        while(theta < t_theta);
+        while(theta < t_theta); 
         
         drive(0, 0);
         msleep(200);
     }
 
-
-    // this one works!!!!! yaaaaayyyy :)))
-    void gyro::turn_angle(int angleDegrees) { // positive is counterclockwise, negative is clockwise
-        double DEG_TO_RAD = 600;
-         double gyroCurrent = 0;
-        
-        int direction = angleDegrees < 0 ? -1 : 1;
-        
-        drive(direction * 300, -direction * 300);
-        
-        if(angleDegrees < 0){
-                while (gyroCurrent >=  angleDegrees * DEG_TO_RAD) { // no point in multiplying angleDegress with direction
-                    gyroCurrent -= std::abs(gyro_z()); // subtract for negative degrees, add for positive degrees
-                    std::cout << gyroCurrent << std::endl;
-                    msleep(10);
-                }
-        }
-        
-        else if(angleDegrees > 0){
-            while (gyroCurrent <=  angleDegrees * DEG_TO_RAD) { // no point in multiplying angleDegress with direction
-                    gyroCurrent += std::abs(gyro_z()); // subtract for negative degrees, add for positive degrees
-                    std::cout << gyroCurrent << std::endl;
-                    msleep(10);
-                }
-        }
-        drive(0, 0);
-         msleep(1000);
-    }
-
     
 //----------------------------------------------------------------------
-     
-    // needs to be fixed and tested
+    
     inline double gyro::correction (double angle){
         
         double c = exp(angle);
@@ -109,48 +96,65 @@ using n_time = std::chrono::high_resolution_clock;
     }
 
 //----------------------------------------------------------------------
-
     
-    auto gyro::duration(){
-        
-        return std::chrono::duration_cast<std::chrono::milliseconds>(c_time - start).count();
-    }
-
-    //unnecessary
     double gyro::c_angle() {
         
         double theta = 0.0;
-        const double con = 2600; //fix conversion might not be accurate
+        const double con = 53800; 
         return theta / con;
         
     }
     
-    // idk if this function works or makes sense
-    void gyro::straight (double distance, double velocity){
+  //  void gyro::straight (double distance, double velocity){
         
-        double i_angle = c_angle();
-        double d_angle = c_angle() - i_angle;
-        double t_time = std::abs(distance / velocity);
+  //      double i_angle = c_angle();
+  //      double d_angle = c_angle() - i_angle;
+  //      double t_time = std::abs(distance / velocity);
         
-        while(duration() < t_time)
-            angle_add();
-        if(c_angle() > i_angle){
-            double speed_1 = velocity * (1 + correction(d_angle));
-            double speed_2 = velocity * (1 - correction(d_angle));
-            drive(speed_1, speed_2);
+  //      while(duration() < t_time)
+  //          angle_add();
+  //      if(c_angle() > i_angle){
+  //          double speed_1 = velocity * (1 + correction(d_angle));
+  //          double speed_2 = velocity * (1 - correction(d_angle));
+  //          drive(speed_1, speed_2);
+  //      }
+  //      else if(c_angle() < i_angle){
+  //          double speed_1 = velocity * (1 - correction(d_angle));
+  //          double speed_2 = velocity * (1 + correction(d_angle));
+  //          drive(speed_1, speed_2);
+  //      }
+        
+  //      drive(0, 0);
+  //  }
+
+    void gyro::turn_angle(int angleDegrees) { // positive is counterclockwise, negative is clockwise
+        double DEG_TO_RAD = 600;
+     	double gyroCurrent = 0;
+        
+        int direction = angleDegrees < 0 ? -1 : 1;
+        
+        drive(direction * 300, -direction * 300);
+        
+        if(angleDegrees < 0){
+       		 while (gyroCurrent >=  angleDegrees * DEG_TO_RAD) { // no point in multiplying angleDegress with direction
+           		 gyroCurrent -= angle_add(); // subtract for negative degrees, add for positive degrees
+           		 std::cout << gyroCurrent << std::endl;
+           		 msleep(10);
+       		 }
         }
-        else if(c_angle() < i_angle){
-            double speed_1 = velocity * (1 - correction(d_angle));
-            double speed_2 = velocity * (1 + correction(d_angle));
-            drive(speed_1, speed_2);
-        }
         
+        else if(angleDegrees > 0){
+        	while (gyroCurrent <=  angleDegrees * DEG_TO_RAD) { // no point in multiplying angleDegress with direction
+           		 gyroCurrent += angle_add(); // subtract for negative degrees, add for positive degrees
+           		 std::cout << gyroCurrent << std::endl;
+           		 msleep(10);
+       		 }      
+        }
         drive(0, 0);
+     	msleep(1000);
     }
 
-
-    //i hope this one works and makes sense
-    	void gyro::angle_straight(int distance, int speed){ //enter distance in cm, it gets multiplied by 95.1 to convert to KIPR units
+	void gyro::angle_straight(int distance, int speed){ //enter distance in cm, it gets multiplied by 95.1 to convert to KIPR units
     	double TO_CM = 95.1; //found motor position ticks per centimeter
         int time = ((TO_CM * distance) / speed) * 1000; // milliseconds
         double currentAngle = 0;
@@ -159,15 +163,16 @@ using n_time = std::chrono::high_resolution_clock;
         while(duration() < time){
         
         if(speed > 0){
-        	double speed_1 = speed * (1 + correction(currentAngle)); //test correction(differenceAngle)
-        	double speed_2 = speed * (1 - correction(currentAngle));// maybe try with normal value first
+            //try with normal values first, like 10
+        	double speed_1 = speed + 10; //speed * (1 + correction(currentAngle)
+        	double speed_2 = speed - 10;// speed * (1 - correction(currentAngle)
         	drive(speed_1, speed_2);
             
         }
         
         else{
-        	double speed_1 = speed * (1 - correction(currentAngle));
-        	double speed_2 = speed * (1 + correction(currentAngle));
+        	double speed_1 = speed - 10; //speed * (1 - correction(currentAngle)
+        	double speed_2 = speed + 10; // speed * (1 + correction(currentAngle)
         	drive(speed_1, speed_2);
             
         }
@@ -175,5 +180,8 @@ using n_time = std::chrono::high_resolution_clock;
             currentAngle += angle_add();
         }
     }
+
+    
+    
 
 
