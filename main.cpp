156cc7e39059cbbf2522670f1e939e79ed943304
ororodislikesphysicalexertion <45035470+ororodislikesphysicalexertion@users.noreply.github.com>
2020-02-13 17:44:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,36 +11,31 @@
 #include "misc.hpp"
 #include <kipr/botball.h>
 
-void initial();
+void eighty(){ // safe, consistent 80 pt function :D
 
-int main(int argc, const char * argv[]) {
-    
-    initial();
-    demobotSeedings();
+	enable_servos();
+	gyro rawr;
    
-  //  std::cout << "Print gyro value: " << rawr.gyro_r() << std::endl;
+    set_servo_position(0, 1937);
     
+      rawr.angle_straight(22, 300); 
+      rawr.turn_angle(132);
+      rawr.angle_straight(10, -300);
+      rawr.turn_angle(40);
+      rawr.angle_straight(24, 300);
+      rawr.turn_angle(120);
+      rawr.turn_angle(-94.5);
     
-    return 0;
-}
+    set_servo_position(0, 1024);
+    msleep(1000);
+    
+    rawr.angle_straight(47, -300);
+    rawr.turn_angle(77);
+    rawr.angle_straight(110, 300);
+    rawr.turn_angle(-75);
+    rawr.angle_straight(35, 300);
+    disable_servos();
 
-void demobotSeedings(){
-    
-    rawr.angle_turn(); // rotate so that demobot is towards coupler
-    rawr.straight_angle(20, 200); // driving towards coupler
-    rawr.angle_turn(pi/4); // pulling coupler out from beneath
-    rawr.straight_angle(20, -200); // backs up with coupler
-    rawr.angle_turn(pi); // rotates with couler towards water poms
-    rawr.straight_angle(40, 300); // drives straight up to the water pom ledge
-
-    rawr.straight_angle(40, -300); // goes back until it is bad at the ramp entrance
-    rawr.angle_turn(pi/4);
-    rawr.straight_angle(10, 150);
-    rawr.angle_turn(-pi/4);
-    rawr.straight_angle(35, 250); // goes up the ramp
-    rawr.angle_turn(-pi/4); // dp I want to use this or a touch sensor? anyways, demobot turns to go on bridge
-    rawr.straight_angle(30, 200); // drives straight slowly while a mechanism pushes water poms into coupler below
-    
 }
 
 void createSeedings(){
@@ -68,4 +63,49 @@ create_connect();
     create_disconnect();
     
 }
+
+void demobotSeedings(){
+
+    enable_servos();
+	gyro rawr;
+    set_servo_position(0, 558);
+    
+      rawr.angle_straight(22, 300); 
+      rawr.turn_angle(132);
+      rawr.angle_straight(10, -300);
+      rawr.turn_angle(40);
+      rawr.angle_straight(24, 300);
+      rawr.turn_angle(83);
+    
+	set_servo_position(0, 1260);
+    
+    rawr.angle_straight(55, 400); 
+    rawr.turn_angle(110);
+    rawr.turn_angle(-33);
+    rawr.angle_straight(5, 200);
+    
+    set_servo_position(0, 440);
+    
+    rawr.angle_straight(30, 200);
+    set_servo_position(0, 1477);
+    msleep(500);
+    disable_servos();
+
+   
+
+}
+
+int main(int argc, const char * argv[]) {
+    
+    initial();
+    wait_for_light();
+    shut_down_in(119);
+    demobotSeedings();
+   
+  //  std::cout << "Print gyro value: " << rawr.gyro_r() << std::endl;
+    
+    
+    return 0;
+}
+
 
